ae0be1965dcbc4338968245a6463fe4b70059e99
はるはるTV <154609555+haruharutv@users.noreply.github.com>
2024-01-05 13:40:26 +0900

    ads

---
+++ haruharutv.js
@@ -0,0 +1,71 @@
+
+if (localStorage.getItem("auth-uuid")) {
+    $.ajax({
+        type: "GET",
+        url: "https://script.google.com/macros/s/AKfycbyGKdY8PhShHp3qvPE9QqELU3boi75y50SLbvRA8K5dL2d472rM6rM2MRsXezWgutFcng/exec",
+        data: { "type": "read_user", "haruharumail": localStorage.getItem("h-mail"), "uuid": localStorage.getItem("auth-uuid") },
+        async: true
+    }).done(function (data) {
+        console.log(data);
+        if (data[5]=!"") { 
+        var newDiv = document.createElement("div");
+        newDiv.style.border = "2px orangered solid";
+        newDiv.style.padding = "20px";
+        newDiv.style.margin = "5px";
+        newDiv.style.userSelect = "none";
+        var boldText = document.createElement("div");
+        boldText.style.fontWeight = "bold";
+        boldText.style.color = "orangered";
+        boldText.style.fontSize = "20px";
+        boldText.textContent = "i 災害情報";
+        newDiv.appendChild(boldText);
+        var paragraph = document.createElement("p");
+        paragraph.textContent = data[5].replace(/|/g, "/n");
+        newDiv.appendChild(paragraph);
+        var link = document.createElement("a");
+        link.href = "https://haruharutv.jp/contact/shindan-tool.html";
+        link.style.fontWeight = "bold";
+        link.style.color = "#000";
+        link.style.textAlign = "center";
+        link.style.display = "block";
+        link.style.backgroundColor = "wheat";
+        link.style.padding = "20px";
+        link.style.borderRadius = "50px";
+        link.style.textDecoration = "none";
+        link.textContent = "気象庁のホームページで詳細を確認する";
+        newDiv.appendChild(link);
+        var targetDiv = document.getElementById("tdiv");
+        targetDiv.appendChild(newDiv);
+        }
+    });
+
+} else {
+    if (getParam("UNM")) {
+        var uname = getParam("UNM");
+    } else {
+        var uname = "guest user";
+
+    }
+    console.log(uname);
+    $.ajax({
+        type: "POST",
+        url: "https://script.google.com/macros/s/AKfycbza4Dw5hgx8weXWJRcVVKpgN873zA90qEIcGKl4E1MK9QbFI5-iHFJ6xsd92Qwxj8iBsw/exec",
+        data: { field1: uname, field2: navigator.userAgent },
+        async: true
+    }).done(function (data) {
+        var parmdata = data.split('|');
+        localStorage.setItem("auth-uuid", parmdata[0]);
+        localStorage.setItem("h-mail", parmdata[1]);
+        localStorage.setItem("usemail", "yes");
+    });
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
